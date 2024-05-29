136c78fc17f3bcb97c56d9c3578f2636f545771f
ctaylor <hi.ctaylor.co@gmail.com>
2024-05-29 15:17:56 -0500

    Added event description comment

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ var button = document.querySelector(".show-cat");
 //console.log(button.outerHTML);
 var cat = document.querySelector(".cat");
 //console.log(cat.outerHTML);
+
+//When user clicks button, function is triggered to show cat image.
 button.addEventListener("click", function () {
     //console.log("It's working");
     cat.classList.add("show");
